805210bc312a8f3ac0de2b8802ad12dc7e8429d9
Leonid Savtchenko <leonid.savtchenko@ucl.ac.uk>
2017-12-18 13:00:19 +0000

    Update path

--- clusterCaSim/host/Core/scripts/win-lin/params.bat
+++ clusterCaSim/host/Core/scripts/win-lin/params.bat
@@ -2,9 +2,9 @@
 
 rem The next parameters must be adjusted before the first launch
 set HEADNODEIP=144.82.46.83
-set LOGIN=savtchenko
-set PASSWORD=h1pp0c
-set HEADNODEWORKERDIR=/home/savtchenko/amcbridge/phase14/nano/clusterCaSim/hpc
+set LOGIN=****
+set PASSWORD=****
+set HEADNODEWORKERDIR=****
 
 rem Other parameters
 set MATLABHOSTDIR=%cd%
